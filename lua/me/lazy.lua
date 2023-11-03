57903feaf46afddf97f2a96ca45410c1fdab2818
Philipp Diercks <philipp.diercks@bam.de>
2023-11-03 18:12:29 +0100

    add basic config with lazy, telescope and colorscheme

---
+++ lua/me/lazy.lua
@@ -0,0 +1,47 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+
+
+	-- Fuzzy Finder (files, lsp, etc)
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.4',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
+			-- Only load if `make` is available. Make sure you have the system
+			-- requirements installed.
+			{
+				'nvim-telescope/telescope-fzf-native.nvim',
+				-- NOTE: If you are having trouble with this installation,
+				--       refer to the README for telescope-fzf-native for more instructions.
+				build = 'make',
+				cond = function()
+					return vim.fn.executable 'make' == 1
+				end,
+			},
+		},
+	},
+	{ 
+		-- Colorscheme
+		'rose-pine/neovim',
+		name = 'rose-pine',
+		priority=1000,
+		config = function()
+			vim.cmd.colorscheme 'rose-pine'
+		end,
+	},
+
+  }, {})
