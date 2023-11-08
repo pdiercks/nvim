9e4969e14e4454fd9b1fd37f40e649b4aa0ebb20
Philipp Diercks <philipp.diercks@bam.de>
2023-11-08 18:09:44 +0100

    add more keymaps for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,4 @@ vim.keymap.set('n', '<leader>sg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>sw', function()
 	builtin.grep_string({ search = vim.fn.input('Grep > ') });
 end)
+vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = '[ ] Find existing buffers' })
