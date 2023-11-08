b8398519fe790332e9116b52ddad3900c9d681c7
Philipp Diercks <philipp.diercks@bam.de>
2023-11-08 18:09:28 +0100

    add more keymaps

--- lua/me/remap.lua
+++ lua/me/remap.lua
@@ -1,5 +1,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- open up netrw
+vim.keymap.set("n", "<leader>rw", vim.cmd.Ex)
+
 -- exit terminal mode with <Esc>
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
