3e5e0c4d332118ddaef59c41b3b78fae73306942
Philipp Diercks <philipp.diercks@bam.de>
2023-11-09 18:28:35 +0100

    add options

---
+++ lua/me/options.lua
@@ -0,0 +1,14 @@
+-- search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- bling
+vim.opt.termguicolors = true
+
+-- behaviour
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
+vim.opt.linebreak = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
