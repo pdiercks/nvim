ad75b2235fae5addb760a10d413af6fdcd796e55
Philipp Diercks <philipp.diercks@bam.de>
2023-11-13 17:08:41 +0100

    add lualine plugin

--- lua/me/lazy.lua
+++ lua/me/lazy.lua
@@ -55,6 +55,19 @@ require("lazy").setup({
 		'numToStr/Comment.nvim',
 		opts = {},
 		lazy = false,
-	}
+    }, 
+    {
+        -- Set lualine as statusline
+        'nvim-lualine/lualine.nvim',
+        -- See `:help lualine.txt`
+        opts = {
+          options = {
+            icons_enabled = false,
+            theme = 'rose-pine',
+            component_separators = '|',
+            section_separators = '',
+          },
+        },
+  },
 
   }, {})
