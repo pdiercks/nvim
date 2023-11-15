acac356424e6f7c52da587554b182d67df79ae88
Philipp Diercks <philipp.diercks@bam.de>
2023-11-15 13:20:53 +0100

    [lazy] add LSP related plugins

--- lua/me/lazy.lua
+++ lua/me/lazy.lua
@@ -68,6 +68,29 @@ require("lazy").setup({
             section_separators = '',
           },
         },
-  },
+    },
+    -- Manage LSP servers and more
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    -- LSP Support
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        lazy = true,
+        config = false,
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+          {'hrsh7th/cmp-nvim-lsp'},
+        }
+    },
+    {
+        -- Autocompletion
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+          {'L3MON4D3/LuaSnip'}
+        },
+    },
 
   }, {})
