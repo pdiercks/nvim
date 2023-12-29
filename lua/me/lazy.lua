03f79c8fa822864cfc4588906d9b82d789a8dde1
Philipp Diercks <philipp.diercks@bam.de>
2023-12-29 18:19:34 +0100

    lazy: format code

--- lua/me/lazy.lua
+++ lua/me/lazy.lua
@@ -1,77 +1,77 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
-	-- the pope
-	{'tpope/vim-fugitive'},
+    -- the pope
+    { 'tpope/vim-fugitive' },
 
-	-- Fuzzy Finder (files, lsp, etc)
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.4',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
-			-- Only load if `make` is available. Make sure you have the system
-			-- requirements installed.
-			{
-				'nvim-telescope/telescope-fzf-native.nvim',
-				-- NOTE: If you are having trouble with this installation,
-				--       refer to the README for telescope-fzf-native for more instructions.
-				build = 'make',
-				cond = function()
-					return vim.fn.executable 'make' == 1
-				end,
-			},
-		},
-	},
-	{ 
-		-- Colorscheme
-		'rose-pine/neovim',
-		name = 'rose-pine',
-		priority=1000,
-	},
-	{
-		-- Highlight, edit, and navigate code
-		'nvim-treesitter/nvim-treesitter',
-		dependencies = {
-			'nvim-treesitter/nvim-treesitter-textobjects',
-		},
-		build = ":TSUpdate",
-	},
-	{
-		-- Comment using gcc, gbc etc.
-		'numToStr/Comment.nvim',
-		opts = {},
-		lazy = false,
-    }, 
+    -- Fuzzy Finder (files, lsp, etc)
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+            -- Only load if `make` is available. Make sure you have the system
+            -- requirements installed.
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                -- NOTE: If you are having trouble with this installation,
+                --       refer to the README for telescope-fzf-native for more instructions.
+                build = 'make',
+                cond = function()
+                    return vim.fn.executable 'make' == 1
+                end,
+            },
+        },
+    },
+    {
+        -- Colorscheme
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        priority = 1000,
+    },
+    {
+        -- Highlight, edit, and navigate code
+        'nvim-treesitter/nvim-treesitter',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        build = ":TSUpdate",
+    },
+    {
+        -- Comment using gcc, gbc etc.
+        'numToStr/Comment.nvim',
+        opts = {},
+        lazy = false,
+    },
     {
         -- Set lualine as statusline
         'nvim-lualine/lualine.nvim',
         -- See `:help lualine.txt`
         opts = {
-          options = {
-            icons_enabled = true,
-            theme = 'rose-pine',
-            component_separators = '|',
-            section_separators = '',
-          },
+            options = {
+                icons_enabled = true,
+                theme = 'rose-pine',
+                component_separators = '|',
+                section_separators = '',
+            },
         },
     },
     -- Manage LSP servers and more
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
     -- LSP Support
     {
         'VonHeikemen/lsp-zero.nvim',
@@ -82,15 +82,17 @@ require("lazy").setup({
     {
         'neovim/nvim-lspconfig',
         dependencies = {
-          {'hrsh7th/cmp-nvim-lsp'},
+            { 'hrsh7th/cmp-nvim-lsp' },
         }
     },
     {
         -- Autocompletion
         'hrsh7th/nvim-cmp',
         dependencies = {
-          {'L3MON4D3/LuaSnip'}
+            { 'L3MON4D3/LuaSnip' }
         },
     },
+    -- Formatter
+    { 'mhartington/formatter.nvim' },
 
-  }, {})
+}, {})
