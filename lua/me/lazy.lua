9246b02c06d00c36970fe249eba90cc9c81c79ce
Philipp Diercks <philipp.diercks@bam.de>
2023-12-29 17:39:37 +0100

    lazy: enable icons in lualine

--- lua/me/lazy.lua
+++ lua/me/lazy.lua
@@ -62,7 +62,7 @@ require("lazy").setup({
         -- See `:help lualine.txt`
         opts = {
           options = {
-            icons_enabled = false,
+            icons_enabled = true,
             theme = 'rose-pine',
             component_separators = '|',
             section_separators = '',
