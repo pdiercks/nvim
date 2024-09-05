f0a822d48f56958a10180166e01b3761fa70e2d0
Philipp Diercks <philipp.diercks@gmail.com>
2024-09-05 13:07:40 +0200

    restructure plugins

---
+++ lua/plugins/all.lua
@@ -0,0 +1,88 @@
+return {
+	-- the pope
+	{ "tpope/vim-fugitive" },
+	{ "tpope/vim-surround" },
+	{ "tpope/vim-repeat" },
+
+	-- Fuzzy Finder (files, lsp, etc)
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
+			-- Only load if `make` is available. Make sure you have the system
+			-- requirements installed.
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				-- NOTE: If you are having trouble with this installation,
+				--       refer to the README for telescope-fzf-native for more instructions.
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+		},
+	},
+	{
+		-- Colorscheme
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+	},
+	{
+		-- Highlight, edit, and navigate code
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		build = ":TSUpdate",
+	},
+	{
+		-- Comment using gcc, gbc etc.
+		"numToStr/Comment.nvim",
+		opts = {},
+		lazy = false,
+	},
+	{
+		-- Set lualine as statusline
+		"nvim-lualine/lualine.nvim",
+		-- See `:help lualine.txt`
+		opts = {
+			options = {
+				icons_enabled = true,
+				theme = "rose-pine",
+				component_separators = "|",
+				section_separators = "",
+			},
+		},
+	},
+	-- Manage LSP servers and more
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	-- LSP Support
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		lazy = true,
+		config = false,
+	},
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-nvim-lsp-signature-help" },
+	-- Completion engine
+	{ "hrsh7th/nvim-cmp" },
+	-- Snippet engine
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			{ "rafamadriz/friendly-snippets" },
+		},
+	},
+	-- Source for nvim-cmp
+	{ "saadparwaiz1/cmp_luasnip" },
+	-- Formatter
+	{ "mhartington/formatter.nvim" },
+	-- Vimtex
+	{ "lervag/vimtex" },
+}
