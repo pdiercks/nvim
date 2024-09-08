3b749f0feb45fdbb36511c53fe343ca52acb83f6
Philipp Diercks <philipp.diercks@gmail.com>
2024-09-08 16:59:52 +0200

    add more colorschemes

---
+++ after/plugin/colorscheme.lua
@@ -0,0 +1,6 @@
+-- setup colorschemes
+require('rose-pine').setup({
+	disable_background = true,
+})
+
+vim.cmd.colorscheme('rose-pine')
---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,5 @@
+return {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "rose-pine/neovim", name = "rose-pine", priority = 1000 },
+    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+}
