84db150cee9416ecbc318e7025a5fdb0927a42be
Philipp Diercks <philipp.diercks@gmail.com>
2024-09-09 22:01:57 +0200

    [remap] cmd.nohl on Esc

--- lua/me/remap.lua
+++ lua/me/remap.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<leader>rw", vim.cmd.Ex)
 
 -- exit terminal mode with <Esc>
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+-- nohl with <Esc> in normal mode
+vim.keymap.set("n", "<Esc>", vim.cmd.nohl)
