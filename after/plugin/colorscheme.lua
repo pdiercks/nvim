59efdf5083df42534c3f9d7cfa290d22eda3e36d
Philipp Diercks <philipp.diercks@gmail.com>
2024-09-11 11:38:31 +0200

    [colorscheme] use bg color for Floats

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,6 +1,14 @@
 -- setup colorschemes
-require('rose-pine').setup({
-	disable_background = true,
+require("rose-pine").setup({
+	variant = "auto",
+	dark_variant = "main",
+	dim_inactive_windows = true,
+	styles = {
+		transparency = true,
+	},
 })
 
-vim.cmd.colorscheme('rose-pine')
+vim.cmd.colorscheme("rose-pine")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
