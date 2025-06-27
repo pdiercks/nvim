f534ffdc72d7ee2b966238abb30009728584a642
Philipp Diercks <philipp.diercks@dlr.de>
2025-06-27 16:00:34 +0200

    add minimal config for sp222

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -3,41 +3,11 @@ return {
 	{ "tpope/vim-fugitive" },
 	{ "tpope/vim-surround" },
 	{ "tpope/vim-repeat" },
-
 	-- Fuzzy Finder (files, lsp, etc)
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.4",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
-			-- Only load if `make` is available. Make sure you have the system
-			-- requirements installed.
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				-- NOTE: If you are having trouble with this installation,
-				--       refer to the README for telescope-fzf-native for more instructions.
-				build = "make",
-				cond = function()
-					return vim.fn.executable("make") == 1
-				end,
-			},
-		},
-	},
-	{
-		-- Highlight, edit, and navigate code
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		build = ":TSUpdate",
-	},
-	{
-		-- Comment using gcc, gbc etc.
-		"numToStr/Comment.nvim",
-		opts = {},
-		lazy = false,
-	},
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 	{
 		-- Set lualine as statusline
 		-- See `:help lualine.txt`
@@ -51,27 +21,4 @@ return {
 			},
 		},
 	},
-	-- LSP Support
-	{ "VonHeikemen/lsp-zero.nvim", branch = "v4.x" },
-	-- Manage LSP servers and more
-	{ "williamboman/mason.nvim" },
-	{ "williamboman/mason-lspconfig.nvim" },
-	{ "neovim/nvim-lspconfig" },
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/cmp-nvim-lsp-signature-help" },
-	-- Completion engine
-	{ "hrsh7th/nvim-cmp" },
-	-- Snippet engine
-	-- {
-	-- 	"L3MON4D3/LuaSnip",
-	-- 	dependencies = {
-	-- 		{ "rafamadriz/friendly-snippets" },
-	-- 	},
-	-- },
-	-- Source for nvim-cmp
-	-- { "saadparwaiz1/cmp_luasnip" },
-	-- Formatter
-	{ "stevearc/conform.nvim", opts = {} },
-	-- Vimtex
-	{ "lervag/vimtex" },
 }
--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,5 +1,3 @@
 return {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     { "rose-pine/neovim", name = "rose-pine", priority = 1000 },
-    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
 }
